aad4c5c328ff2e99a87237e95c7c70e5116dd61d
Softh Axi <softhaxi@gmail.com>
2020-06-10 18:08:28 +0800

    Add dummy product service

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app.route('/', methods=['GET'])
 def index(): 
     return '<h2>Welcome to OnanPlus web service</h2>'
 
+app.route('/api/v1/product', methods=['GET'])
+def products():
+    return '<h2>List of product</h2>'
+
 if __name__ == '__main__':
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True)
