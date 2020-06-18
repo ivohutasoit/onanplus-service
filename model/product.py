8576af066783416f7a2482328eb3f1182ca21683
Softh Axi <softhaxi@gmail.com>
2020-06-18 11:01:34 +0800

    Add company field

--- model/product.py
+++ model/product.py
@@ -6,12 +6,15 @@ class Product(database.Model):
     id = database.Column('PDID00', database.Integer, primary_key=True, nullable=False)
     barcode = database.Column('PDBRCD', database.String(100), nullable=False)
     name = database.Column('PDNAME', database.String(256), nullable=False)
+    company = database.Column('PDCMPY', database.String(100))
     detail = database.Column('PDDTIL', database.String(256))
     prices = {}
 
-    def __init__(self, barcode, name):
+    def __init__(self, barcode, name, company, detail):
         self.barcode = barcode
         self.name = name
+        self.company = company
+        self.detail = detail
 
     def __repr__(self):
         return '<Product {}>'.format(self.barcode)
@@ -21,6 +24,7 @@ class Product(database.Model):
             'id': self.id, 
             'barcode': self.barcode,
             'name': self.name,
+            'company': self.company,
             'detail': self.detail
         }
 
