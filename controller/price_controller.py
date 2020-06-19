102cce80034a83ff57ccfc70fe6fb3c372eb4bce
Softh Axi <softhaxi@gmail.com>
2020-06-19 19:28:19 +0800

    Implement detail price by id

--- controller/price_controller.py
+++ controller/price_controller.py
@@ -23,25 +23,27 @@ def create():
     except Exception as e:
         return jsonify({ 'status': 'ERROR', 'error': str(e) }), 500
 
-# @price_controller.route('/search', methods=['POST'])
-# def search(): 
-#     try:
-#         prices=Price.query.filter_by(product_id=product).\
-#             order_by(Price.date.desc()).all()
-#         return jsonify({ 'status': 'OK', 'prices': json.dumps([price.serialize() \
-#             for price in products]) }), 200
-#     except Exception as e:
-#         return jsonify({ 'status': 'ERROR', 'error': str(e) }), 404
-
-@price_controller.route('/<product>/<store>', methods=['GET'])
-def search(product, store):
+@price_controller.route('/<id>', methods=['GET'])
+def detail(id): 
     try:
-        price=Price.query.filter_by(product_id=product).\
-            filter_by(store_id=store).\
-                order_by(Price.date.desc()).first()
-        if price: 
-            return jsonify({ 'status': 'OK', 'price': price.serialize() }), 200
+        price=Price.query.filter_by(id=id).\
+            order_by(Price.date.desc()).first()
 
+        if price:
+            return jsonify({ 'status': 'OK', 'price': price.serialize() }), 200
+        
         return jsonify({ 'status': 'NOT FOUND'}), 404
     except Exception as e:
-        return jsonify({ 'status': 'ERROR', 'error': str(e) }), 404
+        return jsonify({ 'status': 'ERROR', 'error': str(e) }), 500
+
+@price_controller.route('/by/<product>/<store>', methods=['GET'])
+def by(product, store):
+    try:
+        prices=Price.query.filter_by(product_id=product).\
+            filter_by(store_id=store).\
+                order_by(Price.date.desc()).all()
+
+        return jsonify({ 'status': 'OK', 'prices': [price.serialize() \
+            for price in price] }), 200
+    except Exception as e:
+        return jsonify({ 'status': 'ERROR', 'error': str(e) }), 500
