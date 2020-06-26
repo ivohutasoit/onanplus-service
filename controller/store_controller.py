8835a4a2184ae412fc0d80d6fd5277dca12f2fec
Raja Sihombing <if09051@gmail.com>
2020-06-26 18:47:06 +0800

    Enhance store service

--- controller/store_controller.py
+++ controller/store_controller.py
@@ -1,6 +1,8 @@
 import json
+import requests;
 
 from flask import Blueprint, jsonify, request
+from geopy import distance
 
 from application import database
 from model import Store
@@ -20,12 +22,14 @@ def index():
 def create():
     try:
         store=Store(
-            code= request.json['code'],
+            code= request.json['code'] or None,
             name=request.json['name'], 
-            online=request.json['online'],
-            website=request.json['website'],
-            longitude=request.json['longitude'],
-            latitude=request.json['latitude']
+            online=request.json['online'] or False,
+            website=request.json['website'] or None,
+            longitude=request.json['longitude'] or None,
+            latitude=request.json['latitude'] or None,
+            source='OWN',
+            source_id=request.json['source_id'] or None
         )
         database.session.add(store)
         database.session.commit()
@@ -55,3 +59,92 @@ def detail(id):
 #         return jsonify({ 'status': 'OK', 'store': store.serialize() if store else '{}'}), 200
 #     except Exception as e:
 #         return jsonify({ 'status': 'ERROR', 'error': str(e) }), 404
+
+#https://ourcodeworld.com/articles/read/1019/how-to-find-nearest-locations-from-a-collection-of-coordinates-latitude-and-longitude-with-php-mysql
+@store_controller.route('/around', methods=['GET'])
+def around():
+    sql = """
+    SELECT id, ( 3959 * acos( cos( radians(37) ) * cos( radians( lat ) ) 
+    * cos( radians( lng ) - radians(-122) ) + sin( radians(37) ) * sin(radians(lat)) ) ) AS distance 
+    FROM markers 
+    HAVING distance < 25 
+    ORDER BY distance 
+    LIMIT 0 , 20;
+    """
+
+    latitude = 3.10568
+    longitude = 101.64696
+    radius = 1 # in KM
+
+    query = """
+    SELECT * FROM (SELECT *, 
+        (((ACOS(
+            sin((3.10568 * pi() / 180)) *
+            sin(("STLATD" * pi() / 180)) + cos((3.10568 * pi() /180 )) *
+            cos(("STLATD" * pi() / 180)) * cos(((101.64696 - "STLONG") * pi()/180)))
+        ) * 180/pi() 
+        ) * 60 * 1.1515 * 1.609344 
+        ) "DISTANCE"
+    FROM "STOR01") AS A
+    WHERE A."DISTANCE" <= 1
+    ORDER BY A."DISTANCE";
+    """
+
+    results = database.session.execute(query)
+    stores = []
+    for item in results:
+        store = Store(
+            code=item[1],
+            name=item[2], 
+            online=item[3],
+            website=item[4],
+            latitude=item[5], 
+            longitude=item[6],
+            source='OWN',
+            source_id=item[8]
+        ) 
+        store.id = item[0]
+        store.distance = item[9]
+        stores.append(store)
+
+    base_url = 'http://overpass-api.de/api/interpreter'
+    # base_url = 'https://places.ls.hereapi.com/places/v1/discover/around'
+    url_query = """
+    [out:json];
+    node(around.center:1000,3.10568,101.64696)[~"."~"."][shop];
+    out meta;
+    """
+
+    response = requests.get(base_url, 
+                        params={'data': url_query})
+                        # params={
+                        #     'at': '3.1059857049166495,101.64676227155495;r=50',
+                        #     'cat': 'shopping',
+                        #     'apiKey': '3pGS1RhTklIcwhc14IwoV9m7miWFNsvLVWsVmCwls6M'
+                        # })
+    elements = response.json()['elements']
+
+    for item in elements:
+        if 'name' not in item['tags']:
+            continue
+
+        store = Store(
+            code=None,
+            name=str(item['tags']['name']).upper(), 
+            latitude=item['lat'], 
+            longitude=item['lon'],
+            online=False,
+            website=None,
+            source='OSM',
+            source_id=str(item['id'])
+        )
+        exist = any(x for x in stores if ((x.latitude == store.latitude) & (x.longitude==store.longitude)))
+        if not exist:
+            store.distance = distance.distance((store.latitude, store.longitude), (latitude, longitude)).km
+            stores.append(store)
+
+    stores.sort(key=lambda x: x.distance, reverse=False)
+                        
+    return jsonify({ 'status': 'OK', 'stores': [store.serialize() \
+            for store in stores]  }), 200
+    # return jsonify({'status': 'OK', 'stores': elements}), 200
--- model/store.py
+++ model/store.py
@@ -1,5 +1,10 @@
 from application import database
 
+# class StoreGroup(Type):
+#     __mapper_args__ = {
+#         'polymorphic_identity': 'STOREGROUP'
+#     }
+
 class Store(database.Model):
     __tablename__ = 'STOR01'
 
@@ -8,19 +13,26 @@ class Store(database.Model):
     name = database.Column('STNAME', database.String(256), nullable=False)
     online = database.Column('STONLN', database.Boolean, default=False)
     website = database.Column('STWBST', database.String(256)) 
-    longitude = database.Column('STLONG', database.Float)
     latitude = database.Column('STLATD', database.Float)
+    longitude = database.Column('STLONG', database.Float)
+    source = database.Column('STSOUR', database.String(256))
+    source_id = database.Column('STSRID', database.String(100))
 
-    def __init__(self, code, name, online, website, longitude, latitude):
+    distance = None
+
+    def __init__(self, name, online, website, longitude, latitude, 
+        code=None, source=None, source_id=None):
         self.code = code
         self.name = name
         self.online = online
         self.website = website
         self.longitude = longitude
         self.latitude = latitude
+        self.source = source
+        self.source_id = source_id
 
     def __repr__(self):
-        return '<Store {}>'.format(self.code)
+        return '<Store {}>'.format(self.name)
 
     def serialize(self):
         return {
@@ -30,5 +42,8 @@ class Store(database.Model):
             'online': self.online,
             'website': self.website,
             'longitude': self.longitude,
-            'latitude': self.latitude
+            'latitude': self.latitude,
+            'source': self.source,
+            'source_id': self.source_id,
+            'distance': self.distance
         }
