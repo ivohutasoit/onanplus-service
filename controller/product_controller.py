7fef10beef952b42413330d8736b8bc400692131
Raja Sihombing <if09051@gmail.com>
2020-06-26 18:47:27 +0800

    Enchange product service

--- controller/product_controller.py
+++ controller/product_controller.py
@@ -1,4 +1,5 @@
 import json
+from datetime import date
 from flask import Blueprint, jsonify, request
 
 from application import database
@@ -14,14 +15,31 @@ def index():
             for product in products] }), 200
     except Exception as e:
         return jsonify({ 'status': 'ERROR', 'error': str(e) }), 500
+
+@product_controller.route('', methods=['POST'])
+def create():
+    try:
+        product=Product(
+            barcode= request.json['barcode'], #'9556404115044',
+            name=request.json['name'], 
+            manufacture=request.json['manufacture'] or None,
+            detail=request.json['detail'] or None,
+        )
+        database.session.add(product)
+        database.session.commit()
+        return jsonify({ 'status': 'OK', 'product': product.serialize() }), 201
+    except Exception as e:
+	    return jsonify({ 'status': 'ERROR', 'error': str(e) }), 500
     
 
 @product_controller.route('/<id>', methods=['GET'])
 def detail(id):
+    """
+    id can be product id or barcode
+    """
     try:
-        product=Product.query.filter((Product.id==int(id)) | \
-            (Product.barcode==id) | \
-                (Product.name==id)).first()
+        product=Product.query.filter((Product.id==int(id)) |\
+            (Product.barcode==id)).first()
         if product:
             return jsonify({ 'status': 'OK', 'product': product.serialize() }), 200
 
@@ -30,27 +48,36 @@ def detail(id):
         return jsonify({ 'status': 'ERROR', 'error': str(e) }), 500
 
 @product_controller.route('/<id>/price', methods=['GET'])
-def prices(id):
+def price(id):
+    """
+    id can be product id or barcode
+    """
     try:
-        prices=Price.query.filter_by(product_id=id).\
-                order_by(Price.date.desc()).all()
+        product=Product.query.filter((Product.id==int(id)) |\
+            (Product.barcode==id)).first()
+
+        prices=Price.query.filter_by(product_id=product.id).\
+            filter_by(active=True).\
+            order_by(Price.start_date.asc(), Price.end_date.desc(), Price.amount.asc()).all()
+
+        # sortedPrices = sorted(prices)
         
         return jsonify({ 'status': 'OK', 'prices': [price.serialize('product') \
             for price in prices] }), 200
     except Exception as e:
         return jsonify({ 'status': 'ERROR', 'error': str(e) }), 500
 
-@product_controller.route('', methods=['POST'])
-def create():
-    try:
-        product=Product(
-            barcode= request.json['barcode'], #'9556404115044',
-            name=request.json['name'], 
-            company=request.json['company'],
-            detail=request.json['detail'],
-        )
-        database.session.add(product)
-        database.session.commit()
-        return jsonify({ 'status': 'OK', 'product': product.serialize() }), 201
-    except Exception as e:
-	    return jsonify({ 'status': 'ERROR', 'error': str(e) }), 500
+# @price_controller.route('/<id>/price/cheaper', methods=['POST'])
+# def cheaper(id):
+#     latitude = request.json['latitude']
+#     longitude = request.json['longitude']
+#     try:
+#         price=Price.query.filter_by(id=id).\
+#             order_by(Price.date.desc()).first()
+
+#         if price:
+#             return jsonify({ 'status': 'OK', 'price': price.serialize() }), 200
+        
+#         return jsonify({ 'status': 'NOT FOUND'}), 404
+#     except Exception as e:
+#         return jsonify({ 'status': 'ERROR', 'error': str(e) }), 500
--- model/product.py
+++ model/product.py
@@ -6,14 +6,14 @@ class Product(database.Model):
     id = database.Column('PDID00', database.Integer, primary_key=True, nullable=False)
     barcode = database.Column('PDBRCD', database.String(100), nullable=False)
     name = database.Column('PDNAME', database.String(256), nullable=False)
-    company = database.Column('PDCMPY', database.String(100))
+    manufacture = database.Column('PDMNFR', database.String(256))
     detail = database.Column('PDDTIL', database.String(256))
     prices = {}
 
-    def __init__(self, barcode, name, company, detail):
+    def __init__(self, barcode, name, manufacture, detail):
         self.barcode = barcode
         self.name = name
-        self.company = company
+        self.manufacture = manufacture
         self.detail = detail
 
     def __repr__(self):
@@ -24,7 +24,7 @@ class Product(database.Model):
             'id': self.id, 
             'barcode': self.barcode,
             'name': self.name,
-            'company': self.company,
+            'manufacture': self.manufacture,
             'detail': self.detail
         }
 
